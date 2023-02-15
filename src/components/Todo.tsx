a7c566a1d836380503a41f3d9e004c3fede51a8c
cheeseNA <kurocat2000@gmail.com>
2023-02-15 15:19:33 +0900

    Implement auth

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,13 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import React from 'react';
+import { ArrowRightOnRectangleIcon } from '@heroicons/react/24/outline';
+import { useProcessAuth } from '../hooks/useProcessAuth';
 
 export const Todo = () => {
-  return <div>Todo</div>;
+  const { logout } = useProcessAuth();
+  return (
+    <div className='flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono'>
+      <ArrowRightOnRectangleIcon onClick={logout} className='h-7 w-7 mt-1 mb-5 text-blue-500 cursor-pointer' />
+    </div>
+  );
 };
